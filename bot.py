9047ba1f23f71f42b9c3d1f478faef2ba48ec954
Temirlan Rsanov <86775981+temirlanr@users.noreply.github.com>
2021-12-11 14:06:41 +0600

    fix

--- bot.py
+++ bot.py
@@ -111,7 +111,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN, 
-                          webhook_url="https://mergertelegrambot.herokuapp.com//" + TOKEN)
+                          webhook_url="https://mergertelegrambot.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
